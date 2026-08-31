dc9c5e0fa9b92efc9326b8a9a0b2cc935ce1f256
Regis Tsafack <regis.tsafack@yahoo.com>
2026-08-31 20:20:43 +0200

    Get list endpoint

---
+++ backend/src/main/java/com/filetransfer/file/FileController.java
@@ -0,0 +1,27 @@
+package com.filetransfer.file;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+
+    private final FileStorageService storageService;
+
+    public FileController(FileStorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping
+    public List<FileResponse> listFiles() {
+        return storageService.findAll().stream()
+                .sorted(Comparator.comparing(FileMetadata::uploadDate).reversed())
+                .map(FileResponse::from)
+                .toList();
+    }
+}
